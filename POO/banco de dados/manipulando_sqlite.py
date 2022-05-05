434077ddfe1ead68e627a0c24f3e4d926f1e2f4e
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-05-05 16:08:41 -0300

    instruções insert

--- POO/banco de dados/manipulando_sqlite.py
+++ POO/banco de dados/manipulando_sqlite.py
@@ -1,4 +1,7 @@
 import sqlite3
+import datetime
+import random
+import time
 
 # Criando uma conexão
 con = sqlite3.connect('treino.db')
@@ -22,7 +25,19 @@ def data_insert():
 
 
 # Criar tabela
-create_table()
-
+# create_table()
 # Inserir linha
-data_insert()
+# data_insert()
+
+# Usando variáveis para inserir dados
+def data_insert_var():
+    new_date = datetime.datetime.now()
+    new_produte = 'Notebook'
+    new_valor = random.randrange(3000, 10000)
+    c.execute("INSERT INTO produtos (date, prod_name, valor) VALUES (?, ?, ?)", (new_date, new_produte, new_valor))
+    con.commit()
+
+
+for i in range(10):
+    data_insert_var()
+    time.sleep(1)
